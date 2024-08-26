a9a74554bc7a6082e7d8ea8e8e2dd66f3d3bb504
Oscar Zapata <141781592+HazdepokeR@users.noreply.github.com>
2024-08-26 15:25:58 -0600

    test repairingjs
    xd

--- js/motor.js
+++ js/motor.js
@@ -1,3 +1,12 @@
 (() => {
     console.log('Ok');
   })();
+
+function GenerarMenuPrincipal(){
+  console.log('--GenerarMenuPrincipal()');
+  let mainboard = document.getElementById('mainboard');
+  let freg = document.createElement('p');
+  freg.textContent="firtregister";
+  mainboard.appendChild();
+  
+}
