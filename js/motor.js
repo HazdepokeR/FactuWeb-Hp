b30bb2768b07affd3d14ba35a95a14a4bd2b023a
Oscar Zapata <141781592+HazdepokeR@users.noreply.github.com>
2024-08-27 15:29:24 -0600

    Update motor.js
    xd

--- js/motor.js
+++ js/motor.js
@@ -26,6 +26,7 @@
 
 //console.log(sample);
 */
+
 const botoncargar = document.getElementById('cargarprogreso');
 botoncargar.addEventListener("click", GenerarMenuPrincipal);
 
@@ -41,29 +42,24 @@ function GenerarMenuPrincipal(){
   // Make sure the DOM element exists
   if (upload) 
   {
-    upload.addEventListener('change', function() {
-      // Make sure a file was selected
-      if (upload.files.length > 0) 
-      {
-        var reader = new FileReader(); // File reader to read the file 
+    if (upload.files.length > 0) 
+    {
+      var reader = new FileReader(); // File reader to read the file 
+      // This event listener will happen when the reader has read the file
+      reader.addEventListener('load', function() {
+      var result = JSON.parse(reader.result); // Parse the result into an object 
         
-        // This event listener will happen when the reader has read the file
-        reader.addEventListener('load', function() {
-          var result = JSON.parse(reader.result); // Parse the result into an object 
-          
-          console.log(result);
-          console.log(result.name);
-          console.log(result.age);
-          console.log(result.occupation);
-        });
-        
-        reader.readAsText(upload.files[0]); // Read the uploaded file
-      }
-    });
+      console.log(result);
+      console.log(result.name);
+      console.log(result.age);
+      console.log(result.occupation);
+      });
+      
+      reader.readAsText(upload.files[0]); // Read the uploaded file
+    }
   }
-
-
 }
+
 
 
   
